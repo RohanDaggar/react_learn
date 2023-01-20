62099a80128897eefe83fe19a920739ac00ba333
RohanDaggar <dagzthegreat@gmail.com>
2023-01-20 19:53:33 +0000

    counter with buttons

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,9 +1,12 @@
 import {useState, useEffect} from 'react'
+
+
+
 const Test = () => {
 
 
-  // const [counter, setCounter] = useState(0);
-  // console.log(counter)
+  const [counter, setCounter] = useState(0);
+  console.log(counter)
 
   // useEffect(() => {
   //   setCounter(counter+1)
@@ -24,13 +27,21 @@ const Test = () => {
   return (
     <div>
       <h1>Test Page</h1>
-        {/* <button type="button">
-          I am a button
-        </button>  */}
-      {/* <button type="button" onClick={()=> {setCounter(counter+1)}}>
-          I am a button
-        </button> */}
-        {/*<img width= "100px" height= "100px" src="https://www.w3schools.com/images/w3schools_green.jpg" /> */}
+        {/* A button that increments the counter */}
+        <button onClick={() => setCounter(counter + 1)}>
+          Increment
+        </button>
+        {/* A button that decrements the counter */}
+        <button onClick={() => setCounter(counter - 1)}>
+          Decrement
+        </button>
+        {/* A button that resets the counter */}
+        <button onClick={() => setCounter(0)}>
+          Reset
+        </button>
+        {/* The counter */}
+        <p>Counter: {counter}</p>
+        {/* <img width= "100px" height= "100px" src="https://www.w3schools.com/images/w3schools_green.jpg" /> */}
     </div>
   )
 }
